26d0b9338be2b20f594a5ce706ec8532aa9a81af
Hibasanaa <sanaahiba296@gmail.com>
2024-08-22 14:30:24 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import store from './store';
+import AddTask from './components/AddTask';
+import ListTask from './components/ListTask';
+
+function App() {
+  return (
+    <Provider store={store}>
+      <div className="App">
+        <h1>ToDo List</h1>
+        <AddTask />
+        <ListTask />
+      </div>
+    </Provider>
+  );
+}
+
+export default App;
